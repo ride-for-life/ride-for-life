d24f1bc83159cae981b6b7973bff36cefb3dcc57
Henry Blevins <heblevi@gmail.com>
2019-03-13 12:55:10 -0400

    Add Theme.js with standard colors

---
+++ src/components/styles/Theme.js
@@ -0,0 +1,11 @@
+export const colors = {
+    white: "#FFFFFF",
+    cloud: "#F7F7FA",
+    turbulence: "#F4F4F6",
+    thunderhead: "#78849E",
+    storm: "#454F63",
+    forest: "#4C6A2D",
+    sky: "#BBD1EB",
+    evening: "#555869",
+    black: "#000000",
+}
