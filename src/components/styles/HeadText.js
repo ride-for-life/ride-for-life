4b0b82e01546cafe1a29b39e2339e498e89ee2ee
Henry Blevins <heblevi@gmail.com>
2019-03-13 14:49:58 -0400

    Restyle HeadText to match design doc

--- src/components/styles/HeadText.js
+++ src/components/styles/HeadText.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
+import { fontFamily } from './Theme.js';
 
 export const HeadText = styled.h1`
-   width: 100%;
-   marginLeft: 24px;
+  width: 100%;
+  max-width: 100%;
+  text-align: left;
+  /* FIXME: This is a hack. Reflow PhoneDiv and use correct width */
+  padding-left: 25px;
+  font-family: ${fontFamily};
 `
